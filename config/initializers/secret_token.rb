0f4346117c960fb4c8770dbdbc01bac83415504f
dxhuy1988 <doxuanhuy@gmail.com>
2013-06-15 09:28:45 +0900

    rails add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookpad::Application.config.secret_token = 'a1816db85a0ad205106679e1a2b75769408c1f2a827088df523cbadbbc1cd098183548712b5012c22dde7c395f646709e247c813188dfa80e526764d3f83266c'
