3fdaba2d548d9eecb3a88a5a7687b30b510887c2
Le Van Nghia <nghialv2607@gmail.com>
2013-06-16 01:03:31 +0900

    change scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '589820887715988', '1445cc809e760841ddaee2fd44f8b967', {scope: 'user_about_me,friends_about_me,user_photos,friends_photos,user_relationships,friends_relationships,user_relationship_details,friends_relationship_details,email,read_friendlists,read_requests,read_stream,manage_friendlists,offline_access,publish_stream'}
+  provider :facebook, '589820887715988', '1445cc809e760841ddaee2fd44f8b967', {scope: 'user_about_me,friends_about_me,user_photos,friends_photos,email,read_stream,offline_access,publish_stream'}
 end
