b36d749672a537894f80742bb475efa0ae4e1fee
Le Van Nghia <nghialv2607@gmail.com>
2013-06-16 02:41:53 +0900

    tmp

--- lib/fbphoto.rb
+++ lib/fbphoto.rb
@@ -3,13 +3,10 @@ module FBPhoto
     from = Time.new(year, 01, 01).to_i
     to = Time.new(year+1, 01, 01).to_i
     api = Koala::Facebook::API.new(access_token)
-    res = api.fql_multiquery(
-      photos: "SELECT object_id, pid, src_big, caption, like_info, src_big_height, src_big_width FROM photo WHERE
-          pid IN (SELECT pid FROM photo_tag WHERE subject=me() AND created > #{from} AND created < #{to}) OR
-          pid IN (SELECT pid FROM photo WHERE owner=me() AND modified > #{from} AND created < #{to})
-        ORDER BY rand() LIMIT 30",
-      comments: "SELECT object_id, text FROM comment WHERE object_id IN (SELECT object_id FROM #photos)"
-      )
+    res = api.fql_multiquery({"photos"=>"SELECT object_id, pid, src_big, caption, like_info, src_big_height, src_big_width FROM photo
+    WHERE pid IN (SELECT pid FROM photo_tag WHERE subject=me() AND created > #{from} AND created < #{to})
+    OR pid IN (select pid FROM photo WHERE owner=me() AND modified > #{from} AND created < #{to})
+    ORDER BY rand() LIMIT 150", "comments"=>"SELECT object_id, text FROM comment WHERE object_id IN (SELECT object_id FROM #photos)"})
     @photos = []
     res["photos"].each do |r|
       width = 400 + 3*[r["like_info"]["like_count"]*10, 100].min
